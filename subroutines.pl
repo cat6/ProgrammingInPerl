ff62fbe6f12d6ef9e4664e6f263f129e975a5762
Brian Neary <bri.neary@gmail.com>
2014-03-28 11:33:51 -0400

    Updated subroutines file
    Added more in-depth syntax examples, including nested arrays.

--- subroutines.pl
+++ subroutines.pl
@@ -5,7 +5,29 @@ use warnings;
 
 sub doesUselessNothing 
 {
-   my $sub = "doesUselessNothing";
+   my $sub = "doesUselessNothing";              # It's a best practice to include a variable storing your sub's name
    print "This is subroutine $sub reporting in.\n";
+   
+   my ($argument) = @_;                         # A common way to get simple arguments in Perl.  There are numerous other ways (and problems).
+   print "Argument: $argument\n";
 }
+
+sub evenHarderUselessNothing
+{
+   my $sub = "evenHarderUselessNothing";   
+   
+   my $arr = @_;
+   my $refArr = @_[0];
+   print "\n\n";
+   print "Print argument traditionally: $arr\n";
+   print "Referenced Array: @$refArr\n"
+}
+
+my @firstArr = qw(1 2 3);                       # Construct an array with "qw" syntax
+my @secondArr = (@firstArr, 4, 5, 6);           # Construct a nested array using traditional syntax
+my $quote = "Everybody makes it until they don't.";
+
+print "Second Array: @secondArr\n";             # Second Array: 1 2 3 4 5 6
+
 doesUselessNothing();
+evenHarderUselessNothing( \@secondArr );
