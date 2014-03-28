95244d734b6e40120902f512c1f3bc58f1946165
Brian Neary <bri.neary@gmail.com>
2014-03-28 11:00:52 -0400

    Added subroutine example
    added a subroutine (sub) example

---
+++ subroutines.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+sub doesUselessNothing 
+{
+   my $sub = "doesUselessNothing";
+   print "This is subroutine $sub reporting in.\n";
+}
+doesUselessNothing();
