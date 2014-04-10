09f52c4f50adc1dc4509f66333874fa0c4779c93
Brian Neary <bri.neary@gmail.com>
2014-04-10 13:31:32 -0400

    New PODExample.pl file
    Adding in a file--PODExample.pl--to demonstrate simply the use of Plain
    Old Documentation, POD, in Perl.

---
+++ PODexample.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+# PODexample.pl - a simple example of using the POD (Plain Old Documentation) documentation format, in Perl.
+use strict;
+use warnings;
+=POD
+=head1 Survival Horror
+
+Survival horror is a subgenre video games inspired by horror fiction that focuses on survival 
+of the character and trying to scare the player(s). Although combat can be a part of the gameplay,
+the player is made to feel less powerful than in typical action games, because of limited ammunition,
+health, speed, or other limitations. The player is also challenged to find items that unlock the
+path to new areas, and solve puzzles at certain locations. Games make use of strong horror themes,
+and the player is often challenged to navigate dark maze-like environments, and react to unexpected
+attacks from enemies.
+Source: http://en.wikipedia.org/wiki/Survival_horror
+=cut
